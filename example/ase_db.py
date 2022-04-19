acce37206c422d8c62244de17ba3a753d88f9c98
Mathias Schreiner <jamas@dtu.dk>
2022-04-19 10:20:48 +0200

    use proper key and rename mol to configuration

--- example/ase_db.py
+++ example/ase_db.py
@@ -2,22 +2,22 @@ from argparse import ArgumentParser
 
 import ase.db
 from ase import Atoms
-from dataloader import Dataloader
-
+from t1x import Dataloader
+from tqdm import tqdm
 
 def main(args):  # pylint: disable=redefined-outer-name
-    dataloader = Dataloader(args.h5_in)
+    dataloader = Dataloader(args.h5file)
 
-    with ase.db.connect(args.db_out) as db:
-        for mol in dataloader:
-            atoms = Atoms(mol["atomic_numbers"])
-            atoms.set_positions(mol["positions"])
+    with ase.db.connect(args.db) as db:
+        for configuration in tqdm(dataloader):
+            atoms = Atoms(configuration["atomic_numbers"])
+            atoms.set_positions(configuration["positions"])
 
             data = (
                 {
-                    "wB97x/6-31G(d).energy": mol["wB97x/6-31G(d).energy"],
-                    "wB97x/6-31G(d).atomization_energy": mol["wB97x/6-31G(d).atomization_energy"],
-                    "wB97x/6-31G(d).forces": mol["wB97x/6-31G(d).forces"],
+                    "wB97x/6-31G(d).energy": configuration["wB97x_6-31G(d).energy"],
+                    "wB97x/6-31G(d).atomization_energy": configuration["wB97x_6-31G(d).atomization_energy"],
+                    "wB97x/6-31G(d).forces": configuration["wB97x_6-31G(d).forces"],
                 },
             )
             db.write(atoms, data=data)
@@ -25,6 +25,8 @@ def main(args):  # pylint: disable=redefined-outer-name
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("h5_in")
-    parser.add_argument("db_out")
+    parser.add_argument("h5file")
+    parser.add_argument("db")
     args = parser.parse_args()
+
+    main(args)
