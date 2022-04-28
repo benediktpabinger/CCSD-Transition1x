77bc43e0fc92cfe224d20cbd368c0c0a9eeaaa9f
Mathias Schreiner <jamas@dtu.dk>
2022-04-28 13:28:43 +0200

    run through data splits

--- example/ase_db.py
+++ example/ase_db.py
@@ -1,3 +1,5 @@
+import json
+import os
 from argparse import ArgumentParser
 
 import ase.db
@@ -5,22 +7,37 @@ from ase import Atoms
 from t1x import Dataloader
 from tqdm import tqdm
 
+
 def main(args):  # pylint: disable=redefined-outer-name
-    dataloader = Dataloader(args.h5file)
+    dataloaders = {
+        "train": Dataloader(args.h5file, "train"),
+        "test": Dataloader(args.h5file, "test"),
+        "val": Dataloader(args.h5file, "val"),
+    }
 
     with ase.db.connect(args.db) as db:
-        for configuration in tqdm(dataloader):
-            atoms = Atoms(configuration["atomic_numbers"])
-            atoms.set_positions(configuration["positions"])
+        for split, dataloader in dataloaders.items():
+            split_idx = []
 
-            data = (
-                {
+            for configuration in tqdm(dataloader):
+                atoms = Atoms(configuration["atomic_numbers"])
+                atoms.set_positions(configuration["positions"])
+
+                data = {
                     "wB97x/6-31G(d).energy": configuration["wB97x_6-31G(d).energy"],
-                    "wB97x/6-31G(d).atomization_energy": configuration["wB97x_6-31G(d).atomization_energy"],
+                    "wB97x/6-31G(d).atomization_energy": configuration[
+                        "wB97x_6-31G(d).atomization_energy"
+                    ],
                     "wB97x/6-31G(d).forces": configuration["wB97x_6-31G(d).forces"],
-                },
+                }
+
+                idx = db.write(atoms, data=data)
+                split_idx.append(idx - 1) # idx are 0-indexed in the dataset but 1-indexed in the database
+
+            json.dump(
+                split_idx,
+                open(os.path.join(os.path.dirname(args.db), split + "_idx.json"), "w"),
             )
-            db.write(atoms, data=data)
 
 
 if __name__ == "__main__":
