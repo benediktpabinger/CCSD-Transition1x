164894c49c691fc6c1a5d70f5310a37032e488c9
Mathias Schreiner <jamas@dtu.dk>
2022-04-28 13:29:22 +0200

    write download script

---
+++ download_t1x.py
@@ -0,0 +1,20 @@
+import os
+from argparse import ArgumentParser
+from urllib.request import urlretrieve
+
+
+def main(args):  # pylint: disable=redefined-outer-name
+    os.makedirs(args.dir) # , exists_ok=True)
+
+    urlretrieve(
+        "https://figshare.com/ndownloader/files/34935780",
+        os.path.join(args.dir, "t1x.h5"),
+    )
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("dir", default='.')
+    args = parser.parse_args()
+
+    main(args)
