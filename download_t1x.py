677ef8f82fbeefecb52a927d512303175e7f1242
Mathias Schreiner <jamas@dtu.dk>
2022-07-25 10:01:21 +0200

    add progressbar

--- download_t1x.py
+++ download_t1x.py
@@ -2,19 +2,39 @@ import os
 from argparse import ArgumentParser
 from urllib.request import urlretrieve
 
+import progressbar
+
+
+class ProgressBar:
+    def __init__(self):
+        self.pbar = None
+
+    def __call__(self, block_num, block_size, total_size):
+        if not self.pbar:
+            self.pbar = progressbar.ProgressBar(maxval=total_size)
+            self.pbar.start()
+
+        downloaded = block_num * block_size
+        if downloaded < total_size:
+            self.pbar.update(downloaded)
+        else:
+            self.pbar.finish()
+
 
 def main(args):  # pylint: disable=redefined-outer-name
-    os.makedirs(args.dir,exist_ok=True)
+    os.makedirs(args.dir, exist_ok=True)
 
+    print(f"Downloading Transition1x data to {args.dir}/Transition1x.h5")
     urlretrieve(
-        "https://figshare.com/ndownloader/files/34935789",
-        os.path.join(args.dir, "t1x.h5"),
+        "https://figshare.com/ndownloader/files/36035789",
+        os.path.join(args.dir, "Transition1x.h5"),
+        ProgressBar()
     )
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("dir", default='.')
+    parser.add_argument("dir", default=".")
     args = parser.parse_args()
 
     main(args)
